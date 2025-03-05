d1561c6572a9d605011523f6e2940369dccef289
antony190599 <antonycayo19@gmail.com>
2025-03-04 23:06:59 -0500

    lab03

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,13 +1,17 @@
 import PropTypes from 'prop-types';
-
 import ContactRow from './ContactRow';
 
-const ContactList = ({ contacts, onContactClick }) => {
+const ContactList = ({ contacts, onContactClick, selectedContact }) => {
   return (
     <div className="contact-list">
       <ul>
         {contacts.map((contact, index) => (
-          <ContactRow key={index} contact={contact} onClick={() => onContactClick(contact)} />
+          <ContactRow
+            key={index}
+            contact={contact}
+            onClick={() => onContactClick(contact)}
+            isSelected={selectedContact && selectedContact.fullname === contact.fullname}
+          />
         ))}
       </ul>
     </div>
@@ -17,6 +21,7 @@ const ContactList = ({ contacts, onContactClick }) => {
 ContactList.propTypes = {
   contacts: PropTypes.arrayOf(PropTypes.object).isRequired,
   onContactClick: PropTypes.func.isRequired,
+  selectedContact: PropTypes.object,
 };
 
 export default ContactList;
